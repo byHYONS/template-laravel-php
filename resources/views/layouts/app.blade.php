8a670c8e96f8caa9b3575c45ac692581d9c94be2
byHYONS <byhyons@gmail.com>
2024-07-11 18:11:45 +0200

    fix: favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="{{ Vite::asset('resources/img/byhyons.png')}}">
+    <link rel="icon" type="image/png" href="{{ Vite::asset('resources/img/byhyons.png') }}">
     <title>byHYONS</title>
     @vite('resources/js/app.js')
 </head>
